70a24d84155a0c77b234d2a0d80655008d122b5f
preet <hkaur1572@gmail.com>
2021-07-20 19:12:36 -0400

    reverse array

---
+++ src/javainterviewquestion/reverseArray.java
@@ -0,0 +1,15 @@
+package javainterviewquestion;
+
+public class reverseArray {
+	public static void main(String[] args) {
+		
+		int[] arr =new int[5];
+		
+		arr[0]=1;
+		arr[1]=5;
+		
+		for (int i =arr.length-1; i>=0; i--) {
+			System.out.println(arr[i]);
+		}
+	}
+}
