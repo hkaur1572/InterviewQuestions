bfbe104553f067f525482f10b058e90bda06a935
hkaur1572 <81838384+hkaur1572@users.noreply.github.com>
2021-07-20 21:02:41 -0400

    arrayClassChanges (#2)
    * arrayClassChanges
    
    * A

--- src/javainterviewquestion/reverseArray.java
+++ src/javainterviewquestion/reverseArray.java
@@ -7,7 +7,12 @@ public class reverseArray {
 		
 		arr[0]=1;
 		arr[1]=5;
+
+		arr[2]=6;
+		arr[3]=2;
+
 		int[] arr1 = {1, 3,8, 9, 5};
+
 		for (int i =arr.length-1; i>=0; i--) {
 			System.out.println(arr[i]);
 		}
